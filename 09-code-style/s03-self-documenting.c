ba860882e3e262c479e66ed85db0b1ab03ca193f
genzj <zj0512@gmail.com>
2019-12-13 13:01:02 +0800

    fix logic bug in count function

--- 09-code-style/s03-self-documenting.c
+++ 09-code-style/s03-self-documenting.c
@@ -10,7 +10,7 @@ size_t count(const char *s)
     {
         while (*s != '\0')
         {
-            if ((*s >= 'a' && *s <= 'z') && (*s >= 'A' && *s <= 'Z'))
+            if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
                 count++;
             s++;
         }
